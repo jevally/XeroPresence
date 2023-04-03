057986e87aa84599fc0614cdcd513b6d7f4c4ce5
Dekirai <keivo1997@gmail.com>
2023-04-03 19:15:32 +0200

    Initial commit

---
+++ XeroTest/MainForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace XeroTest
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            bt_login = new Button();
+            tb_sessionid = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            tb_friendnickname = new TextBox();
+            timer1 = new System.Windows.Forms.Timer(components);
+            cb_ShowLevel = new CheckBox();
+            SuspendLayout();
+            // 
+            // bt_login
+            // 
+            bt_login.Location = new Point(12, 134);
+            bt_login.Name = "bt_login";
+            bt_login.Size = new Size(272, 23);
+            bt_login.TabIndex = 0;
+            bt_login.Text = "Start Presence";
+            bt_login.UseVisualStyleBackColor = true;
+            bt_login.Click += button1_Click;
+            // 
+            // tb_sessionid
+            // 
+            tb_sessionid.Location = new Point(12, 36);
+            tb_sessionid.Name = "tb_sessionid";
+            tb_sessionid.Size = new Size(272, 23);
+            tb_sessionid.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(91, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Enter session id:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 62);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Friend Nickname:";
+            // 
+            // tb_friendnickname
+            // 
+            tb_friendnickname.Location = new Point(12, 80);
+            tb_friendnickname.Name = "tb_friendnickname";
+            tb_friendnickname.Size = new Size(272, 23);
+            tb_friendnickname.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            timer1.Interval = 5000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // cb_ShowLevel
+            // 
+            cb_ShowLevel.AutoSize = true;
+            cb_ShowLevel.Location = new Point(12, 109);
+            cb_ShowLevel.Name = "cb_ShowLevel";
+            cb_ShowLevel.Size = new Size(159, 19);
+            cb_ShowLevel.TabIndex = 5;
+            cb_ShowLevel.Text = "Show Level as LargeAsset";
+            cb_ShowLevel.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(300, 164);
+            Controls.Add(cb_ShowLevel);
+            Controls.Add(label2);
+            Controls.Add(tb_friendnickname);
+            Controls.Add(label1);
+            Controls.Add(tb_sessionid);
+            Controls.Add(bt_login);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "MainForm";
+            Text = "Xero Presence";
+            FormClosing += Form1_FormClosing;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button bt_login;
+        private TextBox tb_sessionid;
+        private Label label1;
+        private Label label2;
+        private TextBox tb_friendnickname;
+        private System.Windows.Forms.Timer timer1;
+        private CheckBox cb_ShowLevel;
+    }
+}
---
+++ XeroTest/MainForm.cs
@@ -0,0 +1,150 @@
+using HtmlAgilityPack;
+using System.ComponentModel;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Xml;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument;
+using DiscordRPC;
+using XeroTest.Properties;
+
+namespace XeroTest
+{
+    public partial class MainForm : Form
+    {
+        private static DiscordRpcClient discord;
+        public bool _discordLoggedIn = false;
+        public MainForm()
+        {
+            InitializeComponent();
+            tb_friendnickname.Text = Settings.Default.nickname;
+            tb_sessionid.Text = Settings.Default.sessionid;
+        }
+
+        private static void InitializeDiscord()
+        {
+            discord.Initialize();
+            discord.SetPresence(new RichPresence()
+            {
+                Timestamps = new Timestamps()
+                {
+                    Start = DateTime.UtcNow.AddSeconds(1)
+                }
+            });
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            tb_friendnickname.ReadOnly = true;
+            tb_sessionid.ReadOnly = true;
+            bt_login.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var _room = "";
+            var _nickname = "";
+            var _level = "";
+            var _experience_raw = "";
+            var _experience = "";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"https://xero.gg/player/{tb_friendnickname.Text}/social");
+
+            Cookie cookie = new Cookie("xero_session_ssl", tb_sessionid.Text);
+            cookie.Domain = "xero.gg";
+            request.CookieContainer = new CookieContainer();
+            request.CookieContainer.Add(cookie);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(response.GetResponseStream());
+
+            HtmlNode node1 = doc.DocumentNode.SelectSingleNode("//*[@id=\"settings-data-container\"]/div[3]/div/div/a/div[2]/div/div[2]");
+            HtmlNode node2 = doc.DocumentNode.SelectSingleNode("//*[@id=\"settings-data-container\"]/div[3]/div/div/a/div[2]/div/div[1]");
+            HtmlNode node3 = doc.DocumentNode.SelectSingleNode("//*[@id=\"settings-data-container\"]/div[3]");
+
+            if (node1 != null)
+                _room = node1.InnerText;
+            else
+                return;
+            if (node2 != null)
+                _nickname = node2.InnerText;
+            if (node3 != null)
+            {
+                string imgHtml = node3.OuterHtml;
+
+                var regex = new Regex(@"\s+(?<attributeName>\S+)\s*=\s*""(?<attributeValue>[^""]*)""");
+                var matches = regex.Matches(imgHtml);
+
+                foreach (Match match in matches)
+                {
+                    string attributeName = match.Groups["attributeName"].Value;
+                    string attributeValue = match.Groups["attributeValue"].Value;
+
+                    if (attributeName.ToLower() == "src")
+                        _level = ($"{attributeValue.Replace("/assets/img/grade/xero/84/", "").Replace(".png", "")}");
+                }
+            }
+
+            HttpWebRequest request2 = (HttpWebRequest)WebRequest.Create($"https://xero.gg/player/{_nickname}");
+            request2.CookieContainer = request.CookieContainer;
+
+            HttpWebResponse response2 = (HttpWebResponse)request2.GetResponse();
+            HtmlDocument doc2 = new HtmlDocument();
+            doc2.Load(response2.GetResponseStream());
+
+            HtmlNode node4 = doc2.DocumentNode.SelectSingleNode("/html/head/meta[5]");
+
+            if (node4 != null)
+            {
+                string imgHtml = node4.OuterHtml;
+
+                var regex = new Regex(@"\s+(?<attributeName>\S+)\s*=\s*""(?<attributeValue>[^""]*)""");
+                var matches = regex.Matches(imgHtml);
+
+                foreach (Match match in matches)
+                {
+                    string attributeName = match.Groups["attributeName"].Value;
+                    string attributeValue = match.Groups["attributeValue"].Value;
+
+                    if (attributeName.ToLower() == "content")
+                    {
+                        _experience_raw = $"{attributeValue.Split(" | ")[2]}";
+                        continue;
+                    }
+                }
+            }
+
+            if (_discordLoggedIn == false)
+            {
+                discord = new DiscordRpcClient("1092449168703901756");
+                InitializeDiscord();
+                _discordLoggedIn = true;
+            }
+
+            if (cb_ShowLevel.Checked)
+                discord.UpdateLargeAsset($"https://xero.gg/assets/img/grade/xero/84/{_level}.png", $"Level {_level}");
+            else
+                discord.UpdateLargeAsset("https://dekirai.crygod.de/rpc/xero/logo.png", $"Xero");
+            _experience = Regex.Replace(_experience_raw, @"\s*\([^)]*\)", "");
+            if (cb_ShowLevel.Checked)
+                discord.UpdateDetails($"{_nickname} | {_experience}");
+            else
+                discord.UpdateDetails($"{_nickname} | Lv. {_level} | {_experience}");
+            discord.UpdateState(_room);
+            DiscordRPC.Button[] buttons = new DiscordRPC.Button[1];
+            buttons[0] = new DiscordRPC.Button { Label = "View Profile", Url = $"https://xero.gg/player/{_nickname}" };
+
+            discord.UpdateButtons(buttons);
+            timer1.Interval = 30000;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Settings.Default.sessionid = tb_sessionid.Text;
+            Settings.Default.nickname = tb_friendnickname.Text;
+            Settings.Default.Save();
+        }
+    }
+}
