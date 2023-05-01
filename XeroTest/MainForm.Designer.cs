11b0730ad7337031421f6127d151bbdcc7b4a91f
Dekirai <keivo1997@gmail.com>
2023-05-01 15:50:58 +0200

    Increased delay to 5 seconds

--- XeroTest/MainForm.Designer.cs
+++ XeroTest/MainForm.Designer.cs
@@ -57,7 +57,7 @@
             // 
             // timer1
             // 
-            timer1.Interval = 3000;
+            timer1.Interval = 5000;
             timer1.Tick += timer1_Tick;
             // 
             // label1
